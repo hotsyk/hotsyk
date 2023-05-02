39085e86df522ed3bce7bf21ceff25afc6376b99
Volodymyr Hotsyk <git@hotsyk.com>
2023-05-02 15:02:31 -0700

    Update update.py -2

--- update.py
+++ update.py
@@ -6,6 +6,6 @@ root = pathlib.Path(__file__).parent.resolve()
 if __name__ == "__main__":
     readme = root / "README.md"
     now = datetime.datetime.utcnow()
-    md1 = f"[![StandWithUkraine](https://raw.githubusercontent.com/vshymanskyy/StandWithUkraine/main/badges/StandWithUkraine.svg)](https://github.com/vshymanskyy/StandWithUkraine/blob/main/docs/README.md)"
+    md1 = f"[![StandWithUkraine](https://raw.githubusercontent.com/vshymanskyy/StandWithUkraine/main/banner-personal-page.svg)](https://github.com/vshymanskyy/StandWithUkraine/blob/main/docs/README.md)"
     #md2 = f"Today is {now.strftime('%B %d, %Y')} (UTC), and it's {}"
     readme.open("w").write(md1)
